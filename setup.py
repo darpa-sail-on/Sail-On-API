2af0e8bd3b655909704c5cb0044ada8654b98e17
rwgdrummer <robertsone@sailon_test_server.pargovernment.net>
2020-11-05 10:43:07 -0500

    versio update

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://gitlab.kitware.com/darpa-sail-on/sail-on",
-    version="0.0.1",
+    version="0.0.2",
     zip_safe=False,
     entry_points={
         "console_scripts": [
